b91812be35ffffb1df64448fc5ea76f450784fb4
Dane Wu <dane@easyship.com>
2022-06-16 13:48:28 +0800

    chore - update the example data amount

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
+20.times do
   Recipe.create!(
     title: Faker::Food.dish,
     content: Faker::Food.description,
